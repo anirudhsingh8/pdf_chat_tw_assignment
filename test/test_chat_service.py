bca7c46ba6278f537f0b390cce87e745448e4ed3
Anirudh <anirudh.singh@thoughtworks.com>
2025-07-27 20:24:17 +0530

    Add overall rag system metrics tests using ragas

--- test/test_chat_service.py
+++ test/test_chat_service.py
@@ -1,6 +1,10 @@
 from services.pdf_chat_service import PdfChatService
 from test.utils.utils import file_to_bytes
-import shutil
+from ragas import EvaluationDataset, evaluate
+from ragas.llms import LangchainLLMWrapper
+from langchain_ollama import OllamaLLM
+from ragas.metrics import LLMContextRecall, Faithfulness, FactualCorrectness
+import pprint
 
 test_db_dir = "./test/test_db"
 
@@ -30,3 +34,51 @@ def test_answer_should_be_chat_history_relevant():
     session_messages = service.session_store[session_id].messages
     assert len(session_messages) != 0
     assert "self" in result.lower() and "host" in result.lower()
+
+# Tests overall metrics using ragas
+def test_overall_pdf_rag_service():
+    session_id = "1234"
+    service = initialize_pdf_service(session_id)
+
+    sample_queries = [
+        "How often is data updated in the system?",
+        "How is pricing structured?",
+        "Where can users learn about new EduTrack features?",
+        "Is there a self-hosted version of EduTrack?",
+    ]
+
+    expected_responses = [
+        "Data is refreshed every hour by default. Real-time syncing options are available for premium clients.",
+        "Pricing is based on the number of enrolled learners per year. Tiered pricing and volume discounts are available for larger institutions.",
+        "Feature updates are published via in-app announcements, email newsletters, and the EduTrack Knowledge Base.",
+        "Yes. EduTrack offers both cloud-hosted SaaS and on-premise deployments for institutions with specific data residency needs.",
+    ]
+
+    dataset = []
+
+    for query, reference in zip(sample_queries, expected_responses):
+        relevant_docs = service.get_retriever(session_id).invoke(query)
+        response = service.answer_query(session_id, query)
+        # Extract the page_content from Document objects to get strings
+        retrieved_contexts = [doc.page_content for doc in relevant_docs]
+        dataset.append(
+            {
+                "user_input":query,
+                "retrieved_contexts":retrieved_contexts,
+                "response":response,
+                "reference":reference
+            }
+        )
+
+    evaluation_dataset = EvaluationDataset.from_list(dataset)
+    llm = OllamaLLM(model="gemma3:4b")
+    evaluator_llm = LangchainLLMWrapper(llm)
+
+    result = evaluate(
+        dataset=evaluation_dataset,
+        metrics=[LLMContextRecall(), Faithfulness(), FactualCorrectness()], 
+        llm=evaluator_llm, 
+        batch_size=4
+    )
+
+    pprint.pprint(result.scores)
